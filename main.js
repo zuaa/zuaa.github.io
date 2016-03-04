2b5149072ab0d1e2d5c14ff023c9bd7973f24367
zuaa <zuaa@163.com>
2016-03-04 15:20:47 +0800

    添加新的测试功能

--- main.js
+++ main.js
@@ -3,32 +3,68 @@
  
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+var app = angular.module('myApp', [
   'ngRoute',
   'dashboard'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/'});
+])
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider.otherwise({ redirectTo: '/' });
 }]);
 
- 
- angular.module('dashboard', ['ngRoute'])
+app.controller('loginCtrl', ['$scope', 'LoginService', function ($scope, LoginService) {
+    LoginService.getPullRequests()
+      .then(function (data) {
+        $scope.user = data;
+      });
+  }])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/dashboard/', {
-    templateUrl: 'template/route/dashboard.html', 
-    controller: 'View1Ctrl' 
-  });
+
+
+//登录服务
+app.factory('LoginService', [
+  '$q', '$http',
+  function ($q, $http) {
+    var getPullRequests = function () {
+      var deferred = $q.defer();
+      var client = hprose.Client.create('http://127.0.0.1:8080', [ "loginIn"]);
+      client.ready(function (stub) {
+        stub.loginIn.idempotent = true;
+        stub.loginIn({ zuaa: 1, psw: "aaaaa" })
+          .then(function (result) {
+            console.log(result)
+            deferred.resolve(result);
+          }, function (e) {
+            deferred.reject(e);
+          });
+      },
+        function (e) {
+          deferred.reject(e);
+        });
+      return deferred.promise;
+    }
+    return { // return factory object
+      getPullRequests: getPullRequests
+    };
+  }]);
+
+angular.module('dashboard', ['ngRoute'])
+
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/dashboard/', {
+      templateUrl: 'template/route/dashboard.html',
+      controller: 'View1Ctrl'
+    });
     $routeProvider.when('/', {
-    templateUrl: 'template/route/dashboard.html', 
-    controller: 'View1Ctrl' 
-  });
-}])
-.controller('View1Ctrl',  function( $scope,$routeParams) {
-      var id = $routeParams.id;
-      //console.log('i am otherDetailCtrl page',id);
-      $scope.title = "aaaaa id :"+id;
-} );
+      templateUrl: 'template/route/dashboard.html',
+      controller: 'View1Ctrl'
+    });
+  }])
+  .controller('View1Ctrl', ['$scope', 'LoginService', function ($scope, LoginService) {
+    LoginService.getPullRequests()
+      .then(function (data) {
+        $scope.loginMsg = data;
+      });
+  }])
  
  
  
